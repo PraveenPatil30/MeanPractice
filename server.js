c5699c7328f4ed8fa6287cd5169c580815507f78
PraveenPatil30 <praveen.patil@cognizant.com>
2017-05-11 07:40:07 +0000

    Test changes in server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
- 
+ app.use(express.static('public'));
 mongoose.connect('mongodb://localhost:27017/mittens');
-app.use(express.static('public'));
+
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!')
 });
